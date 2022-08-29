6607a2f55ecc4e8865efcddc9d90783ac00697f0
Shaun <shaunjiji123@gmail.com>
2022-08-29 19:43:26 +0000

    Added new tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,9 +2,13 @@ const assert = require('chai').assert;
 const tail = require('../tail');
 
 describe("#tail", () => {
-  const words = ["Yo Yo", "Lighthouse", "Labs"];
-  it("returns ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
-    assert.deepEqual(tail(words), ['Lighthouse', 'Labs']);
+
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
   });
-  
+
+  it("returns [2, 3] when given [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
 });
